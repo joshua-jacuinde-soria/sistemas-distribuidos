f661701c8cbde86aed978d4a8dbeb673e73f7d1b
Joshua Soria <joshua.jacuinde@cua.uam.mx>
2024-11-13 08:29:26 -0600

    biblioteca creada

---
+++ api.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+from bd_biblioteca import libros
+
+app = FastAPI()
+
+#Metodo: GET
+#URL; '/'
+@app.get('/')
+def bienvenida():
+    print("Atendiendo GET / ")
+    respuesta = {"mensaje": "Bienvenido"}
+    return respuesta
+
+#Metodo Get
+#URL '/libros'
+#devuelva la lista de libros
+@app.get('/libros')
+def lista_libros():
+    print("Atendiendo GET '/libros'")
+    respuesta = libros
+    return respuesta
+
+#Metodo GET
+#URL '/libros/{id}'
+#devuelve un json
+#parametro de ruta id
+@app.get('/libros/{id}')
+def informacion_libro(id:int):
+    print("Atendiendo GET /libros/",id)
+    if id >=0 and id <=len(libros)-1:
+        respuesta = libros[id]
+    else:
+        respuesta = {
+            "mensaje":"El libro no existe"
+        }
+    return respuesta
+
+#Método DELETE
+#URL '/libros/{id}'
+#devuelve un mensaje
+@app.delete('/libros/{id}')
+def eliminar_libro(id:int):
+    print("Atendiendo DELETE /libros/",id)
+    if id >=0 and id <=len(libros)-1:
+        respuesta = {
+            "mensaje": "El libro ha sido eliminado"
+        }
+        libros.pop(id)
+    else:
+        respuesta = {
+            "mensaje": "El libro no existe"
+        }
+    return respuesta
---
+++ bd_biblioteca.py
@@ -0,0 +1,72 @@
+libros = [
+    {
+    "id_libro": 0,
+    "titulo": "La Nausea",
+    "unidades": 10,
+    "autor": "Jean Paul Sartre",
+    "unidades_disponibles":True},
+{
+    "id_libro": 1,
+    "titulo": "Una habitación propia",
+    "unidades": 8,
+    "autor": "Virginia Wolf",
+    "unidades_disponibles":False},
+{
+    "id_libro": 2,
+    "titulo": "El pájaro espino",
+    "unidades": 2,
+    "autor": "Colleen McCullough",
+    "unidades_disponibles": True},
+{
+    "id_libro": 3,
+    "titulo": "El primer hombre",
+    "unidades": 12,
+    "autor": "Albert Camus",
+    "unidades_disponibles": False}
+]
+
+prestamos = [
+    {
+        "id_prestamo": 0,
+        "id_usuario": 0,
+        "id_libro" : 0
+    },
+    {
+        "id_prestamo": 1,
+        "id_usuario": 1,
+        "id_libro" : 1
+    },
+    {
+        "id_prestamo": 2,
+        "id_usuario": 0,
+        "id_libro" : 3
+    },
+    {
+        "id_prestamo": 3,
+        "id_usuario": 1,
+        "id_libro" : 2
+    },
+    {
+        "id_prestamo": 4,
+        "id_usuario": 2,
+        "id_libro" : 1
+    }
+]
+
+usuarios = [
+    {
+        "id_usuario": 0,
+        "nombre": "Flor Juárez",
+        "direccion": "Av. Vasco de Quiroga S/N"
+    },
+    {
+        "id_usuario": 1,
+        "nombre": "Juan Pérez",
+        "direccion": "Av. Constituyentes #1200"
+    },
+    {
+        "id_usuario": 2,
+        "nombre": "Itzel Martínez",
+        "direccion": "Calle 13 #66"
+    }
+]
