b6e3a7297e69d7aec646aa63c26bab910b66868e
Joshua Soria <joshua.jacuinde@cua.uam.mx>
2024-11-19 10:37:43 -0600

    cambios a base de datos

--- api.py
+++ api.py
@@ -2,8 +2,25 @@ from fastapi import FastAPI
 from bd_biblioteca import libros, usuarios
 from uvicorn import run
 from pydantic import BaseModel
+from psycopg import connect
 
 app = FastAPI()
+
+#conexion a la base de datos de postgresql
+DB_NAME = "sistemas_distribuidos"
+DB_USER = "postgres"
+DB_PASS = "JoshuaSoria1"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+
+async def connect_db():
+    return await connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS,
+        port=DB_PORT
+    )
 
 #Metodo: GET
 #URL; '/'
@@ -17,82 +34,100 @@ def bienvenida():
 #URL '/libros'
 #devuelva la lista de libros
 @app.get('/libros')
-def lista_libros():
+async def lista_libros():
     print("Atendiendo GET '/libros'")
-    respuesta = libros
-    return respuesta
+    conn = await connect_db()
+    try:
+        query = "SELECT * FROM libros;"
+        rows = await conn.fetch(query)
+        items = [dict(row) for row in rows]
+        return items
+    finally:
+        await conn.close()
+
 
 #Metodo GET
 #URL '/libros/{id}'
 #devuelve un json
 #parametro de ruta id
 @app.get('/libros/{id}')
-def informacion_libro(id:int):
+async def informacion_libro(id:int):
     print("Atendiendo GET /libros/",id)
-    if id >=0 and id <=len(libros)-1:
-        respuesta = libros[id]
-    else:
-        respuesta = {
-            "mensaje":"El libro no existe"
-        }
-    return respuesta
+    conn = await connect_db()
+    try:
+        query = f"SELECT * FROM libros WHERE id_libro = {id};"
+        rows = await conn.fetch(query)
+        if len(rows) > 0:
+            libro = dict(rows[0])
+        else:
+            libro = {"mensaje": "El libro no existe"}
+        return libro
+    finally:
+        await conn.close()
+    
 
 #Método DELETE
 #URL '/libros/{id}'
 #devuelve un mensaje
 @app.delete('/libros/{id}')
-def eliminar_libro(id:int):
+async def eliminar_libro(id:int):
     print("Atendiendo DELETE /libros/",id)
-    if id >=0 and id <=len(libros)-1:
-        respuesta = {
-            "mensaje": "El libro ha sido eliminado"
-        }
-        del libros[id]
-    else:
-        respuesta = {
-            "mensaje": "El libro no existe"
-        }
-    return respuesta
+    conn = await connect_db()
+    try:
+        query = f"DELETE FROM libros WHERE id_libro = {id};"
+        await conn.execute(query)
+        respuesta = {"mensaje": "Libro eliminado"}
+        return respuesta
+    finally:
+        await conn.close()
 
 #Método Get a usuarios
 #URL '/usuarios'
 #devuelve la lista de usuarios
 @app.get('/usuarios')
-def lista_usuarios():
-    print("Atendiendo GET '/usuarios'")
-    respuesta = usuarios
-    return respuesta
+async def lista_usuarios():
+    print("Atendiendo GET /usuarios")
+    conn = await connect_db()
+    try:
+        query = "SELECT * FROM usuarios;"
+        rows = await conn.fetch(query)
+        items = [dict(row) for row in rows]
+        return items
+    finally:
+        await conn.close()
 
 #Método Get a un solo usuario
 #URL '/usuarios/{id}'
 #devuelve un json
 @app.get('/usuarios/{id}')
-def informacion_usuario(id:int):
+async def informacion_usuario(id:int):
     print("Atendiendo GET /usuarios/",id)
-    if id >=0 and id <=len(usuarios)-1:
-        respuesta = usuarios[id]
-    else:
-        respuesta = {
-            "mensaje":"El usuario no existe"
-        }
-    return respuesta
+    conn = await connect_db()
+    try:
+        query = f"SELECT * FROM usuarios WHERE id_usuario = {id};"
+        rows = await conn.fetch(query)
+        if len(rows) > 0:
+            usuario = dict(rows[0])
+        else:
+            usuario = {"mensaje": "El usuario no existe"}
+        return usuario
+    finally:
+        await conn.close()
 
 #Método DELETE a un usuario
 #URL '/usuarios/{id}'
 #devuelve un mensaje
 @app.delete('/usuarios/{id}') 
-def eliminar_usuario(id:int):
+async def eliminar_usuario(id:int):
     print("Atendiendo DELETE /usuarios/",id)
-    if id >=0 and id <=len(usuarios)-1:
-        respuesta = {
-            "mensaje": "El usuario ha sido eliminado"
-        }
-        del usuarios[id]
-    else:
-        respuesta = {
-            "mensaje": "El usuario no existe"
-        }
-    return respuesta
+    conn = await connect_db()
+    try:
+        query = f"DELETE FROM usuarios WHERE id_usuario = {id};"
+        await conn.execute(query)
+        respuesta = {"mensaje": "Usuario eliminado"}
+        return respuesta
+    finally:
+        await conn.close()
 
 class LibroBase(BaseModel):
     titulo: str
@@ -111,33 +146,35 @@ class UsuarioBase(BaseModel):
 #Método POST a libros
 #URL '/libros'
 @app.post('/libros')
-def agregar_libro(libro: LibroBase):
+async def agregar_libro(libro: LibroBase):
     print("Atendiendo POST /libros")
-    nuevo_libro = {}
-    nuevo_libro['titulo'] = libro.titulo
-    nuevo_libro['unidades'] = libro.unidades
-    nuevo_libro['autor'] = libro.autor
-    nuevo_libro['unidades_disponibles'] = libro.unidades_disponibles
-    libros.append(nuevo_libro)
-    respuesta = {
-        "mensaje": "Libro agregado",
-        "libro": nuevo_libro
-    }
-    return respuesta
+    conn = await connect_db()
+    try:
+        query = f"INSERT INTO libros (titulo, unidades, autor, unidades_disponibles) VALUES ('{libro.titulo}', {libro.unidades}, '{libro.autor}', {libro.unidades_disponibles});"
+        await conn.execute(query)
+        respuesta = {
+            "mensaje": "Libro agregado",
+            "libro": libro
+        }
+        return respuesta
+    finally:
+        await conn.close()
 
 # Put '/libros/{id}'
 # orden de parametros: primero los de ruta y luego los de cuerpo
 @app.put('/libros/{id}')
-def actualizar_disponibilidad_libro(id:int, libro:LibroBase):
+async def actualizar_disponibilidad_libro(id:int, libro:LibroBase):
     print("Atendiendo PUT /libros/",id)
-    libros[id].titulo = libro.titulo
-    libros[id].unidades = libro.unidades
-    libros[id].autor = libro.autor
-    libros[id].unidades_disponibles = libro.unidades_disponibles
-    respuesta = {
-        "mensaje": "Libro actualizado",
-        "libro": libros[id]
-    }
-    return respuesta    
+    conn = await connect_db()
+    try:
+        query = f"UPDATE libros SET unidades_disponibles = {libro.unidades_disponibles} WHERE id_libro = {id};"
+        await conn.execute(query)
+        respuesta = {
+            "mensaje": "Libro actualizado",
+            "libro": libro
+        }
+        return respuesta
+    finally:
+        await conn.close()    
 
 run(app, host='localhost', port=5000)
